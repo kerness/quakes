94d7e9278ee89b99785e7a0091bdc9f79100d0c8
kerness <maciej.bak260@gmail.com>
2022-10-19 21:58:57 +0200

    change everything: remove viewset, use apiView; option to filter by vendor; option to return quakes in specified area;

--- quakes/views.py
+++ quakes/views.py
@@ -1,46 +1,43 @@
-from argparse import Action
-from rest_framework import viewsets, status
-from .models import Quake
-from .serializers import QuakeSerializer
-from rest_framework.decorators import action
-from rest_framework.response import Response
+from quakes.models import Quake
+from quakes.serializers import QuakeSerializer
+from rest_framework import generics
 from django.contrib.gis.geos import Point
 from django.contrib.gis.measure import D
+import logging
 
-class QuakeViewSet(viewsets.ModelViewSet):
+# TODO: zwracanie z konkretnego przedziału dat
+# Może zrobić tak, żeby USGS, GRSS miały całkowicie inne endpointy - quakes/usgs, quakes/grss
+
+class QuakeDetail(generics.RetrieveUpdateDestroyAPIView):
+    """ Return a single Quake by PK """
     queryset = Quake.objects.all()
     serializer_class = QuakeSerializer
+
+class QuakeList(generics.ListAPIView):
+    """View to list all quakes or quakes by specified vendor"""
+    serializer_class = QuakeSerializer
+    logger = logging.getLogger(__name__)
     
 
-    # @action(detail=True)
-    # def quakes_by_vendor(self, request, v=None):
-    #     quakes = Quake.objects.filter(vendor=v)
-    #     quakes_json = QuakeSerializer(quakes)
-    #     return Response(quakes_json.data, many=True)
+    def get_queryset(self):
+        """Pass vendor to get quakes provided by this vendor"""
+        queryset = Quake.objects.all()
+        vendor_name = self.request.query_params.get('vendor')
+        lat = self.request.query_params.get('lat')
+        lng = self.request.query_params.get('lng')
+        radius = self.request.query_params.get('radius')
 
-    @action(detail=False)
-    def highest_mag(self, request):
-        vendor = Quake.objects.all().order_by('mag')
-
-        page = self.paginate_queryset(vendor)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-        
-        serializer = self.get_serializer(vendor, many=True)
-        return Response(serializer.data)
+        self.logger.debug(f'Given parameters: lat: {lat}, lng: {lng}, radius: {radius}')
 
 
-    @action(detail=False, methods=['get'])
-    def closest_hospitals(self, request):
-        """Get hospitals that are at least 3 km from a given location"""
-        longitude = request.GET.get('lon', None)
-        latitude = request.GET.get('lat', None)
+        if vendor_name is not None:
+            queryset = queryset.filter(vendor__exact=vendor_name)
+        elif lat and lng and radius is not None:
+            point = Point(float(lat), float(lng), srid=4326)
+            queryset = Quake.objects.filter(geom__distance_lte=(point, D(km=float(radius))))
 
-        if longitude and latitude:
-            user_location = Point(float(longitude), float(latitude), srid=4326)
-            closest_hospitals = Quake.objects.filter(geom__distance_lte=(user_location, D(km=3)))
-            serializer = self.get_serializer_class()
-            serialized_hospitals = serializer(closest_hospitals, many=True)
-            return Response(serialized_hospitals.data, status=status.HTTP_200_OK)
-        return Response(status.HTTP_400_BAD_REQUEST)
+
+        return queryset
+
+
+
