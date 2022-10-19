f3bbb4704ae2833fdb10eb5a9a9b586ee73608ad
kerness <maciej.bak260@gmail.com>
2022-10-19 21:57:06 +0200

    remove router, manual urls config

--- quakes/urls.py
+++ quakes/urls.py
@@ -1,10 +1,21 @@
-from rest_framework.routers import DefaultRouter
+# from rest_framework.routers import DefaultRouter
 
-from .views import QuakeViewSet
+# from .views import QuakeViewSet
 
-router = DefaultRouter()
+# router = DefaultRouter()
 
-router.register(prefix='api/v1/quakes', viewset=QuakeViewSet, basename='quake')
+# router.register(prefix='api/v1/quakes', viewset=QuakeViewSet, basename='quake')
 
-urlpatterns = router.urls
+# urlpatterns = router.urls
 
+from django.urls import path
+from quakes import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+urlpatterns = [
+    path('quakes/', views.QuakeList.as_view()),
+    path('quakes/<int:pk>/', views.QuakeDetail.as_view()),
+
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
