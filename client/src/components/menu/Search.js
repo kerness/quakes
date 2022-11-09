5f5180f76a7089910824e5a5cb31879185887bd6
kerness <maciej.bak260@gmail.com>
2022-11-09 14:18:22 +0100

    front: font, stupid styles

--- client/src/components/menu/Search.js
+++ client/src/components/menu/Search.js
@@ -17,21 +17,21 @@ const Search = ( { getQuery }) => {
     return (
         // TODO: zrobić tak żeby data druga nie mogła być wcześniejsza od pierwszej
         <form className="search-form" onSubmit={handleSubmit(onSubmit)}>
-            <label htmlFor="start-date">Data początkowa:</label>
+            <label htmlFor="start-date">data początkowa:</label>
             <input type="date" id="start-date" name="start-date"
                 { ...register('startdate', { required: false }) }
                 aria-invalid={errors.startdate ? "true" : "false"}
             />
             { errors.startdate?.type === "required" && <p role="alert">Data początkowa jest wymagana.</p> }
 
-            <label htmlFor="end-date">Data końcowa:</label>
+            <label htmlFor="end-date">data końcowa:</label>
             <input type="date" id="end-date" name="end-date"
                 { ...register('enddate', { required: false }) }
                 aria-invalid={errors.enddate ? "true" : "false"}
             />
             { errors.enddate?.type === "required" && <p role="alert">Data końcowa jest wymagana.</p> }
 
-            <label htmlFor="min-mag">Minimalna magnituda:</label>
+            <label htmlFor="min-mag">minimalna magnituda:</label>
             <input type="number" id="min-mag" name="min-mag"
                 {...register('minmag', { required: false, min: 0.1, max: 10 })}
                 aria-invalid={errors.minmag ? "true" : "false"}
@@ -39,22 +39,22 @@ const Search = ( { getQuery }) => {
             { errors.minmag?.type === "required" && <p role="alert">Podaj minimalną wartość magnitudy.</p> }
 
 
-            <label htmlFor="max-mag">Maksymalna magnituda:</label>
+            <label htmlFor="max-mag">maksymalna magnituda:</label>
             <input type="number" id="max-mag" name="max-mag"
                 {...register('maxmag', { required: false, min: 0.1, max: 10 })}
                 aria-invalid={errors.maxmag ? "true" : "false"}
             />
             { errors.maxmag?.type === "required" && <p role="alert">Podaj maksymalną wartość magnitudy.</p> }
 
-            <label htmlFor="lng">Szerokość geograficzna:</label>
+            <label htmlFor="lng">szerokość geograficzna:</label>
             <input type="number" id="lng" name="lng" { ...register('lng', { required: false }) }/>
 
 
-            <label htmlFor="lat">Długość geograficzna:</label>
+            <label htmlFor="lat">długość geograficzna:</label>
             <input type="number" id="lat" name="lat" {...register('lat', { required: false })} />
 
 
-            <label htmlFor="radius">Promień:</label>
+            <label htmlFor="radius">promień:</label>
             <input type="number" id="radius" name="radius" { ...register('radius', { required: false }) }/>
             
             <input type="submit" id="submit" value="Szukaj" />
