6b75fe1da87f7f60ca1676432db082e67cdd8d8d
kerness <maciej.bak260@gmail.com>
2022-11-16 11:54:59 +0100

    make CC independent of USGSFetcher

--- data_fetchers/USGS/USGSFetcher.py
+++ data_fetchers/USGS/USGSFetcher.py
@@ -4,12 +4,12 @@ Results are exported to GeoJSON file.
 """
 
 
-
 import requests
 import pandas as pd
 import geopandas as gpd
 from pathlib import Path
 import datetime
+
 
 # for testing in developoment
 try:
@@ -17,16 +17,13 @@ try:
 except ModuleNotFoundError:
     DATA_DIR = "../../data"
 
-
-
 # LEVELS and PERIODS are based on the USGS API specification
 MODES = {'feed', 'fdsnws'}
 LEVELS = {'1.0', '2.5', '4.5', 'significant', 'all'}
 PERIODS = {'hour', 'day', 'week', 'month'}
 # URL for creating the requests - {} will be filled with the values from LEVELS nad PERIODS sets
 
-URL_FDSNWS = "https://earthquake.usgs.gov/fdsnws/event/1/{}?format=geojson&starttime={}&endtime={}"
-URL_FEED_COUNT = "https://earthquake.usgs.gov/fdsnws/event/1/count?starttime=2019-04-01&endtime=2019-05-01"
+URL_FDSNWS = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={}&endtime={}"
 URL_FEED = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/{}_{}.geojson"
 
 
@@ -41,31 +38,30 @@ class USGSFetcher:
         PERIODS = {'hour', 'day', 'week', 'month'}
     """
 
-    def __init__(self, mode='feed', level='all', period='day', starttime='2022-11-11', endtime='2022-11-12', count=False):
+    def __init__(self, mode='feed', level='all', period='day', starttime='2022-11-11', endtime='2022-11-12'):
         if level not in LEVELS:
             raise ValueError("Bad level value. Choose one from: " + str(LEVELS))
         if period not in PERIODS:
             raise ValueError("Bad period value. Choose one from: " + str(PERIODS))
         if mode not in MODES:
             raise ValueError("Bad mode value. Choose one from: " + str(MODES))
-        self.count = count
+
+        self.mode = mode
 
         if mode == 'feed':
             self.request_url = URL_FEED.format(level, period)
             self.name = f"{level}{period}"
         else:
-            if count:
-                self.request_url = URL_FDSNWS.format('count', starttime, endtime)
-            else:
-                self.request_url = URL_FDSNWS.format('query', starttime, endtime)
+            self.starttime = starttime
+            self.endtime = endtime
+            self.request_url = URL_FDSNWS.format(starttime, endtime)
             self.name = f"{mode.upper()}{starttime}{endtime}"
-
-
+        
         try:
             r = requests.get(self.request_url)
         except requests.exceptions.RequestException as e:
             raise IOError(e)
-        
+        print(self.request_url)
         self.data = r.json()
 
     def __str__(self):
@@ -74,11 +70,14 @@ class USGSFetcher:
     def __len__(self):
         return len(self.data["features"])
 
-    def fetchData(self):
+
+
+    def transformData(self):
         """
             Fetches the data to GeoPandas dataframe.
         """
         df = gpd.GeoDataFrame()
+        
         for i, feature in enumerate(self.data['features']):
             
             coordinates = feature["geometry"]["coordinates"]
@@ -93,16 +92,11 @@ class USGSFetcher:
 
     def exportData(self,):
         """
-            Writes data to geojson file.
-        """
-        if not self.count:
-            df = self.fetchData()
-            identifier = str(datetime.datetime.now()).replace(" ", "-")
-            path = Path(EXPORT_PATH / f"USGS_{identifier}_{self.name}.geojson")
-            df.to_file(path, driver="GeoJSON")
-            return path
-        else:
-            return(list(self.data.values())[0])
-
-
+            Download, transform and write data to geojson file.
+        """       
+        df = self.transformData()
+        identifier = str(datetime.datetime.now()).replace(" ", "-")
+        path = Path(EXPORT_PATH / f"USGS_{identifier}_{self.name}.geojson")
+        df.to_file(path, driver="GeoJSON")
+        return path
 
--- data_fetchers/USGS/USGSQuakesCounter.py
+++ data_fetchers/USGS/USGSQuakesCounter.py
@@ -1,80 +1,17 @@
-from .USGSFetcher import USGSFetcher
+import data_fetchers.USGS.dateRangeOperations as dro
 from datetime import datetime
 from datetime import timedelta
-import numpy as np
-import datetime as dt
-
-import dateRangeOperations as dro
+import requests
 
 
-# def dateRange(start="2000-01-01", end="2000-12-31"):
-#     # https://stackoverflow.com/questions/63637444/python-month-start-and-end-dates-between-two-dates
-#     start_date = np.datetime64(start[:-3])
-#     # Add one month to end date so last month is included
-#     end_date = np.datetime64(end[:-3]) + np.timedelta64(1, "M")
-#     # Create Date Range
-#     date_range = np.arange(start_date, end_date, dtype="datetime64[M]")
-#     # Add 5 AM to start_dates
-#     start_dates = date_range + np.timedelta64(5, "h")
-#     # Add 1 Month and subrtract 4 hours to 8 PM on last day of month
-#     end_dates = date_range + np.timedelta64(1, "M") - np.timedelta64(4, "h")
-#     # Apply Formatting
-#     final_start_dates = [
-#         parse_date(np.datetime_as_string(d, unit="s")) for d in start_dates
-#     ]
-#     final_end_dates = [
-#         parse_date(np.datetime_as_string(d, unit="s")) for d in end_dates
-#     ]
+def downloadData(start, end):
+    url = f"https://earthquake.usgs.gov/fdsnws/event/1/count?format=geojson&starttime={start}&endtime={end}"
+    try:
+        r = requests.get(url)
+    except requests.exceptions.RequestException as e:
+        raise IOError(e)
 
-#     return final_start_dates, final_end_dates
-
-
-# def parse_date(date_string):
-#     # parse numpy date string
-#     date_time = date_string.split("T")
-#     date_parts = date_time[0].split("-")
-#     time_parts = date_time[1][:-1].split(":")
-#     # convert string values to ints
-#     datetime_ints = [int(d) for d in date_parts] + [int(time_parts[0])]
-
-#     # apply formatting
-#     return dt.datetime(*datetime_ints).strftime("%Y-%m-%d")
-
-
-# def prepare_month_ranges(start, end):
-#     start, end = dateRange(start, end)
-#     month_ranges = []
-#     for s, e in zip(start, end):
-#         month_ranges.append((s, e))
-#     return month_ranges
-
-
-def count_quakes(start, end):
-    over_limit_ranges = []
-    month_ranges = dro.prepare_month_ranges(start, end)
-    for range in month_ranges:
-        fetcher = USGSFetcher(
-            "fdsnws", starttime=range[0], endtime=range[1], count=True
-        )
-        res = fetcher.exportData()
-        print("Zakres dat: ", range, "Count:", res)
-        if res > 20000:
-            over_limit_ranges.append(range)
-            print("!!!!!!!!!!!!!!!!!!!ZA DUŻO!!!!!!!!!!!!!!!!!!!!!")
-
-    over_limit_ranges_count(over_limit_ranges)
-
-
-# def split_date_range(start, end, intv):
-#     """Divide a date range into intervals"""
-#     from datetime import datetime
-
-#     start = datetime.strptime(start, "%Y-%m-%d")
-#     end = datetime.strptime(end, "%Y-%m-%d")
-#     diff = (end - start) / intv
-#     for i in range(intv):
-#         yield (start + diff * i).strftime("%Y-%m-%d")
-#     yield end.strftime("%Y-%m-%d")
+    return list(r.json().values())[0]
 
 
 def over_limit_ranges_count(over_limit_ranges):
@@ -100,13 +37,25 @@ def over_limit_ranges_count(over_limit_ranges):
 
     # pętla do wykonania zapytań w trzech okresach któ©e na siebie nie nachodzą
     for mdr in month_4_add1:
-        print(mdr)
-        fetcher = USGSFetcher("fdsnws", starttime=mdr[0], endtime=mdr[1], count=True)
-        res = fetcher.exportData()
+        #print(mdr)
+        res = downloadData(mdr[0], mdr[1])
         print("Zakres dat: ", mdr[0], mdr[1], "Count:", res)
-        fetcher = USGSFetcher("fdsnws", starttime=mdr[2], endtime=mdr[3], count=True)
-        res = fetcher.exportData()
+        res = downloadData(mdr[2], mdr[3])
         print("Zakres dat: ", mdr[2], mdr[3], "Count:", res)
-        fetcher = USGSFetcher("fdsnws", starttime=mdr[4], endtime=mdr[5], count=True)
-        res = fetcher.exportData()
+        res = downloadData(mdr[4], mdr[5])
         print("Zakres dat: ", mdr[4], mdr[5], "Count:", res)
+
+
+def count_quakes(start, end):
+    over_limit_ranges = []
+    month_ranges = dro.prepare_month_ranges(start, end)
+    for range in month_ranges:
+        res = downloadData(range[0], range[1])
+        print("Zakres dat: ", range, "Count:", res)
+        if res > 20000:
+            over_limit_ranges.append(range)
+            print("\tOver 20000. Spliting into smaller intervals.")
+            over_limit_ranges_count([range])
+
+    
+
