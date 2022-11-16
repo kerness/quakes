e4a45d10f53052a7b298375759901e684c587464
kerness <maciej.bak260@gmail.com>
2022-11-16 14:22:43 +0100

    refactoring - move loader to seperate file

---
+++ data_fetchers/USGS/utils/dbLoad.py
@@ -0,0 +1,25 @@
+import json
+from quakes.models import Quake
+from django.contrib.gis.geos import Point
+from datetime import datetime
+from django.utils.timezone import make_aware
+
+
+
+
+def load_to_django_db(file):
+    geojs = json.loads(file.read_text())
+    for feature in geojs["features"]:
+
+        q = Quake(
+            mag=feature["properties"]["mag"],
+            date=make_aware(datetime.fromtimestamp(feature["properties"]["time"]/1000)).date(), # EDITED: get only date!
+            # może wcześniej zamienic na to takie datetime
+            #date="2022-01-17 18:50:52",
+            geom=Point(
+                feature["geometry"]["coordinates"][0],
+                feature["geometry"]["coordinates"][1],
+            ),
+            vendor='USGS',
+        )
+        q.save()
