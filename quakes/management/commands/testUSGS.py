959615766138ca6a31a023cd2b669223cc7bacff
kerness <maciej.bak260@gmail.com>
2022-11-17 22:45:07 +0100

    date range parameters for exportUSGS

--- quakes/management/commands/testUSGS.py
+++ quakes/management/commands/testUSGS.py
@@ -19,8 +19,8 @@ def test_QueryMode():
     f = uf.USGSFetcher(mode='fdsnws', starttime='2021-01-01', endtime='2021-02-01')
     f.exportData()
 
-def test_quakes_exporter():
-    uqe.export_quakes('2010-03-01', '2010-06-30')
+def test_quakes_exporter(start, end):
+    uqe.export_quakes(start, end)
     #uqe.export_quakes('2021-01-01', '2021-04-01')
 
 
@@ -30,8 +30,15 @@ def test_quakes_exporter():
 class Command(BaseCommand):
     help = "Loads data from USGS"
 
+    def add_arguments(self, parser):
+        parser.add_argument('--start', type=str, required=True)
+        parser.add_argument('--end', type=str, required=True)
+        
+
     def handle(self, *args, **options):
         #test_count_quakes()
         #test_QueryMode()
-        test_quakes_exporter()
+        #test_quakes_exporter()
+        print(options['start'], options['end'])
+        test_quakes_exporter(options['start'], options['end'])
         self.stdout.write(self.style.SUCCESS("TESTS COMPLETED!"))
